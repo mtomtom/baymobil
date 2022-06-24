506a0ca2619d74c222c2c0ec43373711dd9dcd25
mtomtom <melissa.tomkins@jic.ac.uk>
2022-06-24 11:41:02 +0100

    Fixed mpi error: some eco2 values were higher than N

--- build/lib/bf_calculator/bf_calculator.py
+++ build/lib/bf_calculator/bf_calculator.py
@@ -1,27 +1,96 @@
 import numpy as np
-import scipy.special as ss
 import pandas as pd
-from decimal import Decimal
 import os
 import warnings
 from tqdm import tqdm
+import multiprocessing as mp
+import sys
 
-## The stirling approximation is used for large read depths, which would return a binomial
-## value of inf
-def stirling_binom(N,n2):
-  if (N == 0) | (n2 == 0): return np.log(1)
-  elif N == n2: return np.log(1)
-  else: return N*np.log(N) - n2*np.log(n2) - (N-n2) * np.log(N-n2) + 0.5 *(np.log(N) - np.log(n2) - np.log(N-n2) -np.log(2 * np.pi))
+## Define the main functions
+def safebeta(N,n, alpha,beta):
+    facterm = 1.0
+    if alpha>1:
+        a = np.arange(1, alpha)
+        facterm = np.prod((n+a)/(N+a))
+    if beta > 1.0:
+        a = np.arange(1, beta)
+        facterm = facterm * np.prod((N-n+a)/(N+alpha+a))
+    return facterm / (N+alpha)
+
+safebeta = np.vectorize(safebeta)
+
+## Function to calculate the posterior ratio
+def fasterpostN2(Nhomo1,nhomo1,Nhomo2,nhomo2,N,n,nmax):
+    N = int(N)
+    alpha1 = nhomo1+1
+    beta1 = Nhomo1-nhomo1+1
+    alpha2 = nhomo2+1
+    beta2 = Nhomo2-nhomo2+1
+    postN2 = np.zeros(N+2)
+    postN2xN2 = np.zeros(N+2)
+    PN2max = -10.0
+    N2max = 0
+    for N2 in np.arange(0,min(N+1,n+nmax+1)):
+        N2 = int(N2)
+        i = N2 + 1
+        n2_min = max(0,N2-n)
+        n2_max = min(N-n, N2)
+        n2_array = np.arange(n2_min, n2_max+1)
+        postN2[i] = np.sum(safebeta(N-N2,n-N2+n2_array,alpha1,beta1) * safebeta(N2,n2_array,alpha2,beta2))
+        postN2[i]=postN2[i]/i
+        if (N2>0) & (postN2[i]>PN2max):
+            PN2max = postN2[i]
+            N2max = N2
+        
+        postN2xN2[i]=postN2[i]*N2
+    
+    sumpostN2=sum(postN2)
+    postN2=postN2/sumpostN2
+    postN2xN2=postN2xN2/sumpostN2
+    logBF21N2 = np.log10(postN2[N2max+1]/postN2[1]) # +1 because of the index
+    meanN2=sum(postN2xN2)
+    results = [meanN2,N2max,logBF21N2]
+    return results
+
+## Data can be passed in three formats: single value, dataframe, file
+## Default case = single values, nmax=10
+def run_bayes_analysis(data_list, nmax=10):
+    ## Detect data types in list and choose the correct process
+    if isinstance(data_list, list): 
+        ## If 6 single values are passed, then return a single BF 
+        if (len(data_list) == 6) & (all([isinstance(item, int) for item in data_list])):
+            Nhomo1,nhomo1,Nhomo2,nhomo2,N,n = data_list
+            [meanN2,N2max,log10BF] = fasterpostN2(Nhomo1,nhomo1,Nhomo2,nhomo2,N,n,nmax)
+            return meanN2, N2max, log10BF
+        ## If 3 dataframes are passed, then process and return a single dataframe
+        if (len(data_list) == 3) & (all([isinstance(item, pd.DataFrame) for item in data_list])):  
+            ## Run analysis
+            [df_hom_eco1, df_hom_eco2, df_het] = data_list
+            results_df = run_bayes_analysis_df(df_hom_eco1, df_hom_eco2, df_het, nmax)
+            return results_df
+        ## If 3 strings are passed, then load in the files and process the data
+        if (len(data_list) == 3) & (all([isinstance(item, str) for item in data_list])):
+            ## Run analysis
+            [df_hom_eco1, df_hom_eco2, het_file] = data_list
+            run_bayes_analysis_files(df_hom_eco1, df_hom_eco2, het_file, nmax)
+        ## If wrong length of list is passed, then print an error
+        if (len(data_list)!= 6 & len(data_list) != 3):
+            print("Error: list should contain either 6 values, 3 dataframes or 3 filepaths.")
+            return None
+    else:
+        print("Data need to be in list format")
+        return False
+
 
 ## Function to check that the files are in the appropriate format
 def check_data(data_file):
     df = pd.read_csv(data_file, low_memory=False, index_col = None)
     ### Check column headings
     cols = df.columns.to_list()
-    check_cols = ['SNP','N','eco1','eco2']
-    if not all((w in check_cols for w in cols)):
-        print("File must be in format: SNP N eco1 eco2")
-        return False
+    check_cols = ['SNP','N','eco2']
+    if not set(check_cols).issubset(cols):
+        print("File must include columns: SNP N eco2")
+        raise ValueError('Files not formatted correctly.')
     ### Check values can be converted to numeric (if not already)
     cols = df.columns.drop(['SNP'])
     try:
@@ -36,33 +105,46 @@ def check_data(data_file):
             print(f"Warning: {len_df - len(df)} Zero values were removed.")
     except:
         raise ValueError('Unable to convert values to numeric.')
+
+    ## Need to check whether any eco2 columns are greater than the N columns, as this will cause the code to crash
+    error_values = len(df.loc[df["eco2"] > df["N"]])
+    if error_values>0:
+        raise ValueError('Error: eco2 values cannot be greater than N')
+    return df
+
+def check_data_df(df):
+    cols = df.columns.to_list()
+    check_cols = ['SNP','N','eco2']
+    if not set(check_cols).issubset(cols):
+        print("File must include columns: SNP N eco2")
+        raise ValueError('Files not formatted correctly.')
+    ### Check values can be converted to numeric (if not already)
+    cols = df.columns.drop(['SNP'])
+    try:
+        df[cols] = df[cols].apply(pd.to_numeric, errors='coerce')
+        len_df = len(df) 
+        df.dropna(inplace=True)
+        if len(df) < len_df:
+            print(f"Warning: {len_df - len(df)} Non numeric values were removed.")
+        len_df = len(df)
+        df = df.loc[df["N"]>0]
+        if len(df) < len_df:
+            print(f"Warning: {len_df - len(df)} Zero values were removed.")
+    except:
+        raise ValueError('Unable to convert values to numeric.')
+
+    ## Need to check whether any eco2 columns are greater than the N columns, as this will cause the code to crash
+    error_values = len(df.loc[df["eco2"] > df["N"]])
+    if error_values>0:
+        raise ValueError('Error: eco2 values cannot be greater than N')
     return df
 
 ## Apply all the functions and calculate the BF
 def calculate_evidence(df):
-    tqdm.pandas()
-    ## Functions output natural logs - convert to log10
-    print("Calculating Bayes Factors...")
-    df["pos"] = df.progress_apply(lambda x:fasterpostN2(x.Nhomo1,x.nhomo1,x.Nhomo2,x.nhomo2,x.N,x.eco2, x.nmax), axis=1 )
+    df["pos"] = df.apply(lambda x:fasterpostN2(x.Nhomo1,x.nhomo1,x.Nhomo2,x.nhomo2,x.N,x.eco2, x.nmax), axis=1 )
     df[['meanN2','N2max','log10BF']] = pd.DataFrame(df.pos.tolist(), index= df.index)
     df.drop(columns=["pos"], inplace=True)
     return df
-
-def calculate_evidence_stirling(df):
-    tqdm.pandas()
-    ## Functions output natural logs - convert to log10
-    print("Calculating Bayes Factors using Stirling approximation...")
-    df.drop(columns=["meanN2","N2max","log10BF"],inplace=True)
-    df["pos"] = df.progress_apply(lambda x:fasterpostN2_stirling(x.Nhomo1,x.nhomo1,x.Nhomo2,x.nhomo2,x.N,x.eco2, x.nmax), axis=1 )
-    df[['meanN2','N2max','log10BF']] = pd.DataFrame(df.pos.tolist(), index= df.index)
-    df.drop(columns=["pos"], inplace=True)
-    return df
-
-### This function will take in 3 float values and display the output to the screen
-def run_bayes_analysis(hom_eco1N:float, hom_eco1n:float, hom_eco2N:float, hom_eco2n:float, hetN: float, hetn: float, nmax):
-    if nmax == "max": nmax = hetN
-    result = fasterpostN2(hom_eco1N, hom_eco1n, hom_eco2N, hom_eco2n, hetN, hetn, nmax)
-    print(f"meanN2: {result[0]}, N2max: {result[1]}, log10 BF: {result[2]}")
 
 def run_bayes_analysis_files(df_hom_eco1:str, df_hom_eco2:str, het_file:str, nmax):
     ## Check that all of the files are in the correct format
@@ -84,85 +166,40 @@ def run_bayes_analysis_files(df_hom_eco1:str, df_hom_eco2:str, het_file:str, nma
             pd.to_numeric(het_file_merged["nmax"], errors='coerce')
         except:
             raise ValueError('Unable to convert nmax value to numeric.')
-    ## If the binomial returns an inf value, this code will raise a warning. We ignore it here, because we will replace these values later
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        df = calculate_evidence(het_file_merged)
-    ## Check for inf values
-    df_inf = df[df["log10BF"]==np.inf]
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        df_inf_results = calculate_evidence_stirling(df_inf)
-    df_no_inf = df[df["log10BF"]!=np.inf]
-    df_new = pd.concat([df_no_inf, df_inf_results],axis=1)
-    df = df_new
+    ## Split the dataframe into chunks to parallelise
+    dfs = np.array_split(het_file_merged, mp.cpu_count() - 1)
+    pool = mp.Pool(processes = (mp.cpu_count() - 1))
+    results = list(tqdm(pool.imap(calculate_evidence, dfs)))
+    pool.close()
+    pool.join()
+    df = pd.concat(results)
     outfile = het_file.split(".")[0] + "_results.csv"
     df.to_csv(outfile, index = None)
-    
-## The main function that handles the Bayesian anaylsis. This takes the parameters: Nhomo1, ## nhomo2, the reads that map to eco1 and eco2 in homograft 1, Nhomo2, nhomo2, the reads ## that map to eco2 and eco1 in homograft 2, N, n, the reads that map to eco1 and eco2 in the heterograft dataset and nmax, the reads from N over which we want to integrate.
 
-def fasterpostN2 (Nhomo1,nhomo1,Nhomo2,nhomo2,N,n,nmax):
-    N = int(N)
-    alpha1 = nhomo1+1
-    beta1 = Nhomo1-nhomo1+1
-    alpha2 = nhomo2+1
-    beta2 = Nhomo2-nhomo2+1
-    postN2 = np.zeros(N+2)
-    postN2xN2 = np.zeros(N+2)
-    PN2max = -10.0
-    N2max = 0
-    for N2 in np.arange(0,min(N+1,n+nmax+1)):
-        N2 = int(N2)
-        i = N2 + 1
-        n2_min = max(0,N2-n)
-        n2_max = min(N-n, N2)
-        for n2 in np.arange(n2_min, n2_max+1):
-            postN2[i] = postN2[i] + ss.binom(N-N2,n-N2+n2)*ss.binom(N2,n2)* ss.beta(n-N2+n2+alpha1,N-n-n2+beta1)*ss.beta(n2+alpha2,N2-n2+beta2)
-            postN2[i]=postN2[i]/i
-        if (N2>0) & (postN2[i]>PN2max):
-            PN2max = postN2[i]
-            N2max = N2
-        
-        postN2xN2[i]=postN2[i]*N2
-    
-    sumpostN2=sum(postN2)
-    postN2=postN2/sumpostN2
-    postN2xN2=postN2xN2/sumpostN2
-    logBF21N2 = np.log10(postN2[N2max+1]/postN2[1]) # +1 because of the index
-    meanN2=sum(postN2xN2)
-    results = [meanN2,N2max,logBF21N2]
-    return results
-
-### The main function using the Stirling approximation for those values with large read depths
-def fasterpostN2_stirling(Nhomo1,nhomo1,Nhomo2,nhomo2,N,n,nmax):
-    N = int(N)
-    alpha1 = nhomo1+1
-    beta1 = Nhomo1-nhomo1+1
-    alpha2 = nhomo2+1
-    beta2 = Nhomo2-nhomo2+1
-    postN2 = np.zeros(N+2)
-    postN2xN2 = np.zeros(N+2)
-    PN2max = -10.0
-    N2max = 0
-    for N2 in np.arange(0,min(N+1,n+nmax+1)):
-        N2 = int(N2)
-        i = N2 + 1
-        n2_min = max(0,N2-n)
-        n2_max = min(N-n, N2)
-        for n2 in np.arange(n2_min, n2_max+1):
-            log_values = Decimal(stirling_binom(N-N2,n-N2+n2) + stirling_binom(N2,n2)+ ss.betaln(n-N2+n2+alpha1,N-n-n2+beta1)+ss.betaln(n2+alpha2,N2-n2+beta2))
-            postN2[i] = Decimal(postN2[N2+1]) + log_values.exp()
-        postN2[i]=postN2[i]/i
-        if (N2>0) & (postN2[i]>PN2max):
-            PN2max = postN2[i]
-            N2max = N2
-        
-        postN2xN2[i]=postN2[i]*N2
-    
-    sumpostN2=sum(postN2)
-    postN2=postN2/sumpostN2
-    postN2xN2=postN2xN2/sumpostN2
-    logBF21N2 = np.log10(postN2[N2max+1]/postN2[1]) # +1 because of the index
-    meanN2=sum(postN2xN2)
-    results = [meanN2,N2max,logBF21N2]
-    return results
+def run_bayes_analysis_df(df_hom_eco1:pd.DataFrame, df_hom_eco2:pd.DataFrame, df_het:pd.DataFrame, nmax):
+    ## Check that all of the dataframes are in the correct format
+    df_het_file = check_data_df(df_het)
+    df_hom_eco1 = check_data_df(df_hom_eco1)
+    df_hom_eco2 = check_data_df(df_hom_eco2)
+    ## Rename the columns in the homograft files
+    df_hom_eco1 = df_hom_eco1.rename(columns={'N': 'Nhomo1', 'eco2': 'nhomo1'})
+    df_hom_eco2 = df_hom_eco2.rename(columns={'N': 'Nhomo2', 'eco2': 'nhomo2'})
+    ## Create a single dataframe with all of the values
+    het_file_merged = pd.merge(df_het, df_hom_eco1[["SNP","Nhomo1","nhomo1"]], on = "SNP")
+    het_file_merged = pd.merge(het_file_merged, df_hom_eco2[["SNP","Nhomo2","nhomo2"]], on = "SNP")
+    ## Add in our nmax values
+    if nmax == "max":
+        het_file_merged["nmax"] = het_file_merged["N"]
+    else:
+        try:
+            het_file_merged["nmax"] = nmax
+            pd.to_numeric(het_file_merged["nmax"], errors='coerce')
+        except:
+            raise ValueError('Unable to convert nmax value to numeric.')
+    ## Split the dataframe into chunks to parallelise
+    dfs = np.array_split(het_file_merged, mp.cpu_count() - 1)
+    pool = mp.Pool(processes = (mp.cpu_count() - 1))
+    results = list(tqdm(pool.imap(calculate_evidence, dfs)))
+    pool.close()
+    pool.join()
+    return pd.concat(results)
